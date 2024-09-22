e2df097e9aa4e24f80b05286c85cdfc358755f9b
Khenziii <khenzii.was.taken@gmail.com>
2024-09-22 15:39:59 +0200

    feat: implement `generateStatement`

--- src/main/java/dev/khenzii/korwin.java
+++ src/main/java/dev/khenzii/korwin.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 public class korwin {
-    public String generateStatement() {
-        return "not yet implemented!";
+    public static String generateStatement() {
+        ArrayList<String> quote = new ArrayList<>();
+
+        for (String[] quotes : data.KORWIN_QUOTES) {
+            Random numberGenerator = new Random();
+            int randomIndex = numberGenerator.nextInt(quotes.length);
+            String randomQuote = quotes[randomIndex];
+
+            quote.add(randomQuote);
+        }
+
+        return String.join(" ", quote);
     }
 }
